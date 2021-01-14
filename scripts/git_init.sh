4a4400984a8870a54b4fa8359f44518dfc2c2c19
Rupert <rbroad@ohpen.com>
2021-01-14 12:50:49 +0100

    feat remove shell scritps

--- scripts/git_init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Connecting an existing repository to a local non-empty folder
-
-git init
-git add .
-git commit -m 'start'
-git remote add origin http://bitbucket.org/ohpen-dev/terraform-aws-ns-ecs
-git push -u origin master
