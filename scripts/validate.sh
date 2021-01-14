5d66b5c5a5a9925956485a9436e5177834f60f9a
Rupert <rbroad@ohpen.com>
2021-01-14 13:41:21 +0100

    switch to terraform image - install tflint

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -2,14 +2,14 @@
 export AWS_DEFAULT_REGION="eu-west-1"
 
 # tflint
-# apk add --update openssl
-# apk add curl
-# tflint_latest_url=$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")
-# wget "$tflint_latest_url"
-# unzip tflint_linux_amd64.zip
-# mkdir -p /usr/local/tflint/bin
-# export PATH=/usr/local/tflint/bin:$PATH
-# install tflint /usr/local/tflint/bin
+apk add --update openssl
+apk add curl
+tflint_latest_url=$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")
+wget "$tflint_latest_url"
+unzip tflint_linux_amd64.zip
+mkdir -p /usr/local/tflint/bin
+export PATH=/usr/local/tflint/bin:$PATH
+install tflint /usr/local/tflint/bin
 tflint 
 
 # validate
