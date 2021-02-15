1f6b497c17b169cb16f11bf650a17d17cf487778
Rupert <rbroad@ohpen.com>
2021-02-15 17:58:27 +0100

    patch update validate script, remove dup install

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -2,14 +2,19 @@
 export AWS_DEFAULT_REGION="eu-west-1"
 
 # tflint
-apk add --update openssl
-apk add curl
-tflint_latest_url=$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")
-wget "$tflint_latest_url"
-unzip tflint_linux_amd64.zip
-mkdir -p /usr/local/tflint/bin
-export PATH=/usr/local/tflint/bin:$PATH
-install tflint /usr/local/tflint/bin
+if ! command -v tflint &> /dev/null
+then
+    echo "tflint could not be found - installing"
+    apk add --update openssl
+    apk add curl
+    tflint_latest_url=$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")
+    wget "$tflint_latest_url" -O tflint_linux_amd64.zip
+    unzip tflint_linux_amd64.zip -o
+    mkdir -p /usr/local/tflint/bin
+    export PATH=/usr/local/tflint/bin:$PATH
+    install tflint /usr/local/tflint/bin
+fi
+
 tflint 
 
 # validate
