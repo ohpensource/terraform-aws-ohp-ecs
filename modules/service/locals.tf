3ae90fae822da286434aa96842009eebae5c5369
Rupert <rbroad@ohpen.com>
2021-05-04 12:07:02 +0200

    lanz-974

--- modules/service/locals.tf
+++ modules/service/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  health_check_grace_period_seconds = ecs_load_balancers != [] ? var.health_check_grace_period_seconds : null
+  health_check_grace_period_seconds = var.ecs_load_balancers != [] ? var.health_check_grace_period_seconds : null
 }
